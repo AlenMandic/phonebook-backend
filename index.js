ef071f8b1224555b9b2b95e1e5cb641b94a833d8
Alen Mandic <alenmandic1@gmail.com>
2023-10-06 00:08:30 +0200

    fixed incorrect port assigment

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/persons', (req, res) => {
 
 app.use(unknownEndpoint)
 
-const port = 3001
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
 })
